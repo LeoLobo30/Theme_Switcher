6a49cf99974ea5fdc87b241aa9a89db8f6e92864
Leonardo Lobo <leonardogomeslobo@gmail.com>
2022-03-20 22:43:16 -0300

    colocando a alteração do backgroundColor e adicionando trasações

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 let button = document.querySelector("button");
 
+/* colors of body
+ --dark: #292C35;
+ --light: #F1F1F1; */
+
 button.addEventListener("click", function () {
 
-  if (button.style.left == "0%" || !button.style.left) {
+  if (button.style.left == "0%") {
     button.style.left = "50%";
+    document.body.style.backgroundColor = "#292C35";
   } else {
     button.style.left = "0%";
+    document.body.style.backgroundColor = "#F1F1F1";
   }
   console.log(button.style.left);
 
